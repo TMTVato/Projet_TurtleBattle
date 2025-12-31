e49bf86d3b82386e9b512ff938d61ec3b29791c9
TMTVato <128924487+TMTVato@users.noreply.github.com>
2025-12-31 17:29:27 +0100

    fixed null ref when using shovel

--- Assets/Marc_Work/survivor/Scripts/Bullet.cs
+++ Assets/Marc_Work/survivor/Scripts/Bullet.cs
@@ -7,17 +7,30 @@ public class Bullet : MonoBehaviour
     [SerializeField] private Tower_shoot Turret;
     float startTime;
 
-
     public float damage;
-    private float per;//penetration
+    private float per; //penetration
 
     Rigidbody2D rb;
+
     private void Awake()
     {
-        damage = Turret.bullet_damage;
-        per = Turret.bullet_penetration;
         rb = GetComponent<Rigidbody2D>();
+        // Correction : Initialiser Turret si non assigné dans l'inspecteur
+        if (Turret == null)
+            Turret = GetComponentInParent<Tower_shoot>();
+
+        // Vérification supplémentaire pour éviter le NullReferenceException
+        if (Turret != null)
+        {
+            damage = Turret.bullet_damage;
+            per = Turret.bullet_penetration;
+        }
+        else
+        {
+            Debug.LogWarning("Turret n'est pas assigné sur le prefab Bullet.");
+        }
     }
+
     public void Init(float damage, float per, Vector3 dir)
     {
         this.damage = damage;
@@ -26,37 +39,33 @@ public class Bullet : MonoBehaviour
         if (per > -1)
         {
             this.rb.velocity = dir * 15f;
-
         }
-
     }
-
-
 
     void Start()
     {
         startTime = Time.time;
 
-        transform.Rotate(Vector3.forward * -90);
-        transform.localScale = Vector3.one * Turret.bullet_size;
-    }
-
-
-
-
-    void Update()
-    {
-        transform.Translate(Vector2.up * Time.deltaTime * Turret.bullet_velocity);
-
-        if (Time.time >= startTime + Turret.bullet_life_time)
+        if (Turret != null)
         {
-            Destroy(gameObject, 0f);
+            transform.Rotate(Vector3.forward * -90);
+            transform.localScale = Vector3.one * Turret.bullet_size;
         }
     }
 
+    void Update()
+    {
+        if (Turret != null)
+        {
+            transform.Translate(Vector2.up * Time.deltaTime * Turret.bullet_velocity);
 
+            if (Time.time >= startTime + Turret.bullet_life_time)
+            {
+                Destroy(gameObject, 0f);
+            }
+        }
+    }
 
-    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!collision.CompareTag("Enemy") || per == -1)
@@ -65,11 +74,9 @@ public class Bullet : MonoBehaviour
         per--;
 
         if (per == -1)
-        {   
+        {
             rb.velocity = Vector2.zero;
             gameObject.SetActive(false);
         }
-
-
     }
 }
